9ac8a0a2b4af189acc03dbbbbcbe389f8f284653
sh1ggy <tyronewessnolasco@gmail.com>
2022-01-18 16:40:18 +1000

    Initial commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,16 @@
 import '../styles/globals.css'
+import Head from 'next/head'
 import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Component {...pageProps} />
+      <Head>
+        <link rel="stylesheet" href="https://unpkg.com/@picocss/pico@latest/css/pico.classless.min.css"/>
+      </Head>
+    </>
+  )
 }
 
 export default MyApp
